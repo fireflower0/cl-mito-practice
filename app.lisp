523b5f9be0c2086e17847f207b82572fbfd548bd
fireflower0 <fireflower0@hotmail.co.jp>
2019-03-10 10:22:37 +0900

    Add a file

---
+++ app.lisp
@@ -0,0 +1,21 @@
+(defpackage :cl-mito-practice/app
+  (:use :cl
+        :mito))
+(in-package :cl-mito-practice/app)
+
+(defparameter *app-root* (asdf:system-source-directory :database-test))
+(defparameter *db-name*  (merge-pathnames #P"db/test.db" *app-root*))
+
+(connect-toplevel :sqlite3 :database-name *db-name*)
+
+(deftable user ()
+  ((name  :col-type (:varchar 64))
+   (email :col-type (or (:varchar 128) :null))))
+
+(table-definition 'user)
+
+(deftable tweet ()
+  ((status :col-type :text)
+   (user   :col-type user)))
+
+(table-definition 'tweet)
---
+++ cl-mito-practice.asd
@@ -0,0 +1,9 @@
+(defsystem "cl-mito-practice"
+  :class :package-inferred-system
+  :version "0.1.0"
+  :author "fireflower0"
+  :license ""
+  :depends-on ("dbi"
+               "mito"
+               "sxql"
+               "cl-mito-practice/app"))
